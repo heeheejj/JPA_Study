4c2c9ccb630236aac1fdfa8116ed1c95c16d4fd1
heeheejj <kjyk0835@naver.com>
2023-08-24 20:26:10 +0900

    add: 엔티티 매핑 - 기본 키 매핑 자동생성 전략

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,9 +6,15 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false)
@@ -32,6 +38,11 @@ public class Member {
     private int temp;
 
     public Member(){
+    }
+
+    public Member(Long id, String username){
+        this.id = id;
+        this.username = username;
     }
 
     public Long getId() {
